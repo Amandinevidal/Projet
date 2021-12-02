6223abe25dcbacc09027b50362c4e38e2fd8bc56
Amandine Vidal <amandine.vidalhosteng@gmail.com>
2021-12-02 16:44:41 +0100

    add make

---
+++ make.R
@@ -0,0 +1,16 @@
+#### --------------------------------------------- ####
+# Date : 02/12/2021
+# Author : Emmanuelle, Bastien, Amandine
+# email : amandine.vidal-hosteng@univ-tlse3.fr
+# Encoding : UTF-8
+# file : make.R
+
+# Main script
+#### --------------------------------------------- ####
+
+#### Load library shiny ####
+library(shiny)
+
+#### Data simulations ####
+#### Model ####
+#### Create Output ####
