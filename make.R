6c60ae95ac0968f1dd62ba8e3c8848727fcf98f0
bmourgui <bastien.mourguiart@mail.com>
2021-12-02 12:13:16 +0100

    Add header capsule

---
+++ make.R
@@ -0,0 +1,9 @@
+#### --------------------------------------------- ####
+# Date : 02/12/2021
+# Author : Emmanuelle, Bastien, Amandine
+# email : amandine.vidal-hosteng@univ-tlse3.fr
+# Encoding : UTF-8
+# file : make.R
+
+# Compile other scripts of the project
+#### --------------------------------------------- ####
