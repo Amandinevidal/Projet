7bf38fd9e7ec2a4bc5d3298a89f51353ccc67134
Amandine Vidal <amandine.vidalhosteng@gmail.com>
2021-12-02 16:38:41 +0100

    Parameters reading with reactive to mcmc file

--- R/shiny.R
+++ R/shiny.R
@@ -15,31 +15,43 @@ library(shiny)
 ui<-fluidPage(titlePanel("Trace Plot visualisation depending on parameter selection"),
 
               sidebarPanel( # add web site options on side
-                fileInput(inputId = "output", # name of file to import (used next in server function)
+                fileInput(inputId = "output1", # name of file to import (mcmc table) not load just named
                           label = "Upload model outputs", # name of the option
                           accept = c(".RData")), # output extension expected
-
-                selectInput(inputId = "nb1", # name of input for parameter choice (used next in server function)
-                            h3("Which parameter ?"), # title for selection
-                            choices = list("parameter alpha"=1, "parameter beta 1"=2,"parameter beta 2"=3,"parameter beta 3"=4,"parameter mu"=5,"paramter sigma"=6), # parameter names
-                            selected = 1)), # default value
-
+                uiOutput("param")
+              ),
               mainPanel( # page setup
                 h2("Trace plot"), # title
                 plotOutput(outputId = "plot"), # plot
                 br(), # empty line
-                br() # empty line
+                br(),# empty line
               )
+
+
 )
 
 
 #### SERVER FUNCTION ####
 
 server <-function(input,output){
-  i <- reactive({as.character(input$nb1)}) # i take one parameter value corresponding to selectInput choices l24
-  output$plot= renderPlot({ # type of element to add in shiny
-    plot(1:10,1:10) # plot
+
+  outvar <- reactive({
+    if(is.null(input$output1)) { return(paste(" "))}
+    else {
+    file1<-load(input$output1$datapath,verbose = T)
+    print(file1)
+    return(rownames(output))
+    }
   })
+  output$param = renderUI({
+    selectInput('param2',"Parameters",outvar())
+  })
+
+  i <- reactive({as.character(input$param2)}) # i take one parameter value corresponding to selectInput choices l24 #
+  output$plot= renderPlot({ # type of element to add in shiny
+    plot(1:10,1:10,main=paste(i())) # plot
+  })
+
 }
 
 #### CALL SHINY APP ####
