2302f953e1e96888c1b15ad4ec9309a3ab674e68
Amandine Vidal <amandine.vidalhosteng@gmail.com>
2021-12-02 16:43:37 +0100

    add files

---
+++ R/model.R
@@ -0,0 +1,10 @@
+#### --------------------------------------------- ####
+# Date : 02/12/2021
+# Author : Emmanuelle, Bastien, Amandine
+# email : amandine.vidal-hosteng@univ-tlse3.fr
+# Encoding : UTF-8
+# file : model.R
+
+# Model simulations
+#### --------------------------------------------- ####
+
