4dd67d6c922e7da69d36af250068d05ba64b8659
bmourgui <bastien.mourguiart@mail.com>
2021-12-02 15:44:15 +0100

    Adapt name for script 1 & 2

--- analyses/01-simu_data.R
+++ analyses/01-simu_data.R
@@ -9,7 +9,7 @@ p_occ3 <- simu_occu(alpha = 0.01, beta = -0.001, elev = elev)$p_occu
 plot(p_occ1~elev) ; plot(p_occ2~elev)
 
 
-dat <- cbind(rbinom(length(p_occ1), 1, p_occ1),
+oc <- cbind(rbinom(length(p_occ1), 1, p_occ1),
             rbinom(length(p_occ2), 1, p_occ2),
             rbinom(length(p_occ3), 1, p_occ3)
 )
