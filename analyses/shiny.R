75f3444ef489b3409d2cb4048d45003db8af8091
Amandine Vidal <amandine.vidalhosteng@gmail.com>
2021-12-03 10:32:03 +0100

    add summry table

--- analyses/shiny.R
+++ analyses/shiny.R
@@ -10,6 +10,7 @@
 
 library(shiny)
 
+
 #### VISUALS ####
 
 ui<-fluidPage(titlePanel("Trace Plot visualisation depending on parameter selection"),
@@ -20,14 +21,18 @@ ui<-fluidPage(titlePanel("Trace Plot visualisation depending on parameter select
                           accept = c(".RData")), # output extension expected
                 uiOutput("param"),
                 # add text advert to know which parameter does not converge
-                uiOutput(outputId = "warning")
+                uiOutput(outputId = "warning"),
+                br(),
+                h2("Parameters summary"),
+                br(),
+                tableOutput("table"),
               ),
               mainPanel( # page setup
                 h2("Trace plot"), # title
-                plotOutput(outputId = "plot"), # trace plot
+                plotOutput(outputId = "plot",width = "100%", height = "300px"), # trace plot
                 br(), # empty line
                 h2("Density posterior for each chains"),
-                plotOutput(outputId = "plot2"),
+                plotOutput(outputId = "plot2",width = "100%", height = "300px"),
                 br(), # empty line
                 br()# empty line
               )
@@ -38,14 +43,17 @@ ui<-fluidPage(titlePanel("Trace Plot visualisation depending on parameter select
 
 server <-function(input,output){
 
+  ## load data
+  filedata <- reactive({
+    file1 <- load(input$output1$datapath,verbose = T)
+    get(file1)
+  })
+
   ## load output1 + save rownames as parma2
   outvar <- reactive({
     if(is.null(input$output1)) { return(paste(" "))} # at the moment there is no file
     else { # when browse
-      file1<-load(input$output1$datapath,verbose = T) # take output and read it in file1
-      results <- MCMCvis::MCMCsummary(jsample)
-      # print(file1) # check if file1 is readed
-      # take only rhat>1.1 rownames
+      results <- MCMCvis::MCMCsummary(filedata()) # create mcmc summary
       return(rownames(results)) # return rownames to add it in menu
     }
   })
@@ -55,14 +63,25 @@ server <-function(input,output){
     selectInput('param2',"All parameters",outvar())
   })
 
+  ## print summary table
+  summarytable <- reactive({
+    MCMCvis::MCMCsummary(filedata())
+  })
+  output$table <- renderTable({
+    if(is.null(input$output1)) {
+      return(paste(" ")) # at the moment there is no file
+    } else {
+      summarytable()
+    }
+  },rownames = T)
+
   ## plot (output2) with reactive title to selected parameter (output1)
   i <- reactive({as.character(input$param2)}) # i take one parameter value corresponding to selectInput choices l24 #
   output$plot= renderPlot({ # type of element to add in shiny
     if(is.null(input$output1)) {
       return(paste(" ")) # at the moment there is no file
     } else {
-      load(input$output1$datapath,verbose = T) # take output and read it in file1
-      bayesplot::mcmc_trace(jsample,pars=i()) # trace plot
+      bayesplot::mcmc_trace(filedata(),pars=i()) # trace plot
     }
   })
 
@@ -71,8 +90,7 @@ server <-function(input,output){
     if(is.null(input$output1)) {
       return(paste(" ")) # at the moment there is no file
     } else {
-      load(input$output1$datapath,verbose = T) # take output and read it in file1
-      bayesplot::mcmc_dens_chains(jsample,pars=i()) # density posterior for each chains
+      bayesplot::mcmc_dens_chains(filedata(),pars=i()) # density posterior for each chains
     }
   })
 
@@ -80,8 +98,7 @@ server <-function(input,output){
     if(is.null(input$output1)) {
       return(paste(" ")) # at the moment there is no file
     } else {
-      load(input$output1$datapath,verbose = T) # take output and read it in file1
-      results2 <- MCMCvis::MCMCsummary(jsample)
+      results2 <- MCMCvis::MCMCsummary(filedata())
       results2nc <- results2[results2$Rhat>1.1,]
       if (is.null(rownames(results2[results2$Rhat>1.1,]))) {
         HTML(
